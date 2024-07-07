ae1df493beed5781f352721e01a4f783ceb4e202
jydnbach <jaydenkim95@gmail.com>
2024-07-08 08:54:46 +0900

    lifted state up in postlist from newpost

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,13 +1,29 @@
+import { useState } from 'react';
+
 import classes from './PostList.module.css';
 import Post from './Post';
 import NewPost from './NewPost';
 
 function PostList() {
+  const [enteredBody, setEnteredBody] = useState('');
+  const [enteredAuthor, setEnteredAuthor] = useState('');
+
+  function bodyChangeHandler(e) {
+    setEnteredBody(e.target.value);
+  }
+
+  function authorChangeHandler(e) {
+    setEnteredAuthor(e.target.value);
+  }
+
   return (
     <>
-      <NewPost />
+      <NewPost
+        onBodyChange={bodyChangeHandler}
+        onAuthorChange={authorChangeHandler}
+      />
       <ul className={classes.posts}>
-        <Post author="Max" body="React is fun" />
+        <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Chris " body="Javascript is also fun" />
       </ul>
     </>
