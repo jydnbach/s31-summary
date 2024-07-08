3807b9c153ff88bdb2921503ea2c303d66e3bd86
jydnbach <jaydenkim95@gmail.com>
2024-07-08 14:01:41 +0900

    root layout

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,17 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import NewPost from './components/NewPost';
+import RootLayout from './routes/RootLayout';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />,
+    element: <RootLayout />,
+    children: [
+      { path: '/', element: <App /> },
+      { path: '/create-post', element: <NewPost /> },
+    ],
   },
-  { path: '/create-post', element: <NewPost /> },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
