2746c1faa8b254414ecbeca3049a1dbc1021a73d
jydnbach <jaydenkim95@gmail.com>
2024-07-08 10:05:55 +0900

    previous state based  update

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -6,11 +6,17 @@ import Post from './Post';
 import NewPost from './NewPost';
 
 function PostList({ isPosting, onStopPosting }) {
+  const [posts, setPosts] = useState([]);
+
+  function addPostHandler(postData) {
+    setPosts((existingPosts) => [postData, ...existingPosts]); //callback to update state
+  }
+
   return (
     <>
       {isPosting && (
         <Modal onClose={onStopPosting}>
-          <NewPost onCancel={onStopPosting} />
+          <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
       <ul className={classes.posts}>
