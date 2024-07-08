40d0062d0fd6cdb0ced875340460037bb6fed1a8
jydnbach <jaydenkim95@gmail.com>
2024-07-08 17:48:15 +0900

    submitting data with Form and action

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -7,17 +7,6 @@ import Post from './Post';
 function PostList({}) {
   const posts = useLoaderData();
 
-  function addPostHandler(postData) {
-    fetch('http://localhost:8080/posts', {
-      method: 'POST',
-      body: JSON.stringify(postData),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    setPosts((existingPosts) => [postData, ...existingPosts]); //callback to update state
-  }
-
   return (
     <>
       {posts.length > 0 && (
